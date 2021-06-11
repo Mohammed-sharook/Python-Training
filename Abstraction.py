bdd9afd64012ecf5017774585df883ce796f8f43
Mohammed-sharook <aydensharu@gmail.com>
2021-06-11 17:19:50 +0530

    second commit

--- Abstraction.py
+++ Abstraction.py
@@ -1,5 +1,40 @@
-# Hiding internal details or implementations and showing only the functionalities
+# from abc import ABC, abstractmethod  # import ABS class from abs module
+#
+#
+# class Abstract(ABC):  # Cannot able to create objects for abstract class
+#     def __init__(self, name):
+#         self.name = name
+#
+#     @abstractmethod
+#     def age(self,number):
+#         print("His age is",number)
+#
+#
+# obj = Abstract("sharook") # can't create obj for abs class
+# obj.age(23)
 
-# Any class with one abstract function is called abstract class
+
+# we can achieve abstraction using inheritance.
+
+from  abc import ABC,abstractmethod
 
 
+class Dog(ABC):
+    def __init__(self, bread):
+        self.bread = bread
+
+    def about_bread(self):
+        print("The dogs which from",self.bread,"are very powerful")
+
+    @abstractmethod
+    def price(self,amount):
+        pass
+
+
+class New_Dog(Dog):
+    def price(self,amount):
+        print("The", self.bread, "price is", amount, "thousand")
+
+
+obj1 = New_Dog("Dollmansion")
+obj1.price(30)
